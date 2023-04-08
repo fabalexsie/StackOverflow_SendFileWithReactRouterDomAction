76097161e51b02031131965ecf5af64cfda4033e
Fabian <50114802+fabalexsie@users.noreply.github.com>
2023-04-08 10:12:15 +0200

    initial react app + server code

---
+++ upload-server/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import multer from 'multer';
+import fs from 'fs';
+
+const app = express();
+
+// serve uploaded files
+app.use('/files', express.static('files'));
+
+const upload = multer({ dest: 'files/' }); // for parsing the uploaded file
+app.use(express.json()); // for parsing the password
+
+app.get('/fileList', (req, res) => {
+    fs.readdir('files/', (err, files) => {
+        res.send(files);
+    });
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    console.log(req.body.pw , req.file);
+    res.send('File uploaded');
+});
+
+app.listen(8080, () => {
+  console.log(`Server listening on port ${8080}`);
+});
